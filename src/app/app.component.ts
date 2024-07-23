4161af9537cc5ed96f4a7f016a0b7f50e4595e74
jaykakkar31 <jaykakkar31@gmail.com>
2024-07-23 23:08:02 +0530

    updated code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,29 +2,51 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../shared/module/wishitems';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Jay';
-  items = [
+  newWishText: string = '';  // Initialize here instead of in constructor
+
+  listFilter: string = '0'
+  items: WishItem[] = [
     new WishItem('Learn Angular'),
-    new WishItem('Get coffeee', true),
+    new WishItem('Get coffee', true),
     new WishItem('Get a job')
-  ]
-  newWishText=""
-  toggleItem(item:WishItem){
-    item.isComplete=!item.isComplete
-    console.log('clicked',item);
+  ];
+  get visibleItems(): WishItem[] {
+    let value=this.listFilter
+    if (value == '0') {
+      // console.log('caled=',this.items)
+      return this.items
+    } else if (value == '1') {
+      return this.items.filter((i)=>!i.isComplete)
+    }else{
+      return this.items.filter((i)=>i.isComplete)
+    }
+  }
+  filterChanged(value: number) {
+    console.log(value)
+
+
+    // console.log(this.visibleItems);
 
   }
-  addNewWish(){
-
+  toggleItem(item: WishItem) {
+    item.isComplete = !item.isComplete;
+    console.log('clicked', item);
   }
 
+  addNewWish() {
+    console.log("-----------------", this.newWishText);
+    this.items.push(new WishItem(this.newWishText));
+    this.newWishText = '';  // Clear the input after adding
+  }
 }
